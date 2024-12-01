46b16ca5f5f0649051b8a6c45e30752f2ed7a7a9
BeatrizSanto <beatriz.santos19@yahoo.com.br>
2024-12-01 18:31:01 -0300

    adicione

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,9 +1,22 @@
-
+import S from './queroDoar.module.scss'
+import livro from '../../assets/livro.png'
 
 export default function QueroDoar(){
     return(
-        <section>
-            <h2>Quero Doar</h2>
+        <section className={S.principal}>
+            <section className={S.container}>
+                <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+                <form action=''>
+                <div>
+                    <img src={livro} alt= '' />
+                    <h3>Informações do livro</h3>
+                </div>
+                <input type="text" placeholder='Titulo' />
+                <input type="text" placeholder='categoria'/>
+                <input type="text" placeholder='Autor' />
+                <input type="text" placeholder='Link da imagem' />
+                </form>
+            </section>
         </section>
     )
-}
+    }
