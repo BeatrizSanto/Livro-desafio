820b9b5e7d1b5f4007621b919f2c233daaab964d
BeatrizSanto <beatriz.santos19@yahoo.com.br>
2024-12-05 07:44:41 -0300

    parte

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,8 +1,18 @@
+import S from './doados.module.scss'
+import livro from '../../assets/livro1.png'
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Doados Livros</h2>
+            <section className={S.boxcard}>
+                <article>
+                    <img src={livro} alt='' />
+                    <h3>O Protagonista</h3>
+                    <p>Susane Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
