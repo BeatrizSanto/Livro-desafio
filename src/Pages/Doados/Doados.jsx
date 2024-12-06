069ec3551216b228d84367d8c07741757cb3ea5c
BeatrizSanto <beatriz.santos19@yahoo.com.br>
2024-12-06 19:09:25 -0300

    Final

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -12,6 +12,24 @@ export default function Doados(){
                     <p>Susane Andrade</p>
                     <p>Ficção</p>
                 </article>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
             </section>
         </section>
     )
