55cca935204e6e48883e464a0d514412f59e3b01
BeatrizSanto <beatriz.santos19@yahoo.com.br>
2025-04-02 18:30:44 -0300

    um parte

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,9 +1,27 @@
 import S from './doados.module.scss'
 import livro from '../../assets/livro1.png'
+import axios from 'axios'
+import {useState,useEffect} from 'react'
+
+
 
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () =>{
+        const response = await axios.get("https://api-livros-1.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(()=>{
+        getLivros()
+    },[])
+
+
+
     return(
-        <section className={S.boxDoados}>
+        <section className={S.boxDoados}> 
             <h2>Doados Livros</h2>
             <section className={S.boxcard}>
                 <article>
@@ -12,24 +30,16 @@ export default function Doados(){
                     <p>Susane Andrade</p>
                     <p>Ficção</p>
                 </article>
-                <article>
-                    <img src={livro} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={livro} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={livro} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
+                {livros.map((item)=>(
+                    <article key={item}>
+                        <img src={item.imagem_url} alt="" />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.categoria}</p>
+                        <p>{item.autor}</p>
+                    </article>
+
+                ))}
+
             </section>
         </section>
     )
